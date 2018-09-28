b229da7329a371e90b6dba4a3d15ed32c665ea1d
Marlon Regenhardt <marlon.r@hotmail.de>
2018-09-28 09:38:30 +0200

    MultiProcessRunner manual tester

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Threading;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Regenhardt
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			using (var runner = new MultiProcessRunner())
 			{
 				var task = new Task(() => { });
-				runner.AddPocess("cmd.exe", task, true);
+				runner.AddPocess("cmd.exe", task);
 				Console.WriteLine("Now waiting");
 
 				task.Wait();
